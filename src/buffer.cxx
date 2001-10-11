cdcb4ed7b9ba16834028ecf16cb410c3b712ee4c
Isaac Foraker <isaac@noscience.net>
2001-10-11 00:22:16 +0000

    save
    git-svn-id: svn+ssh://hobbit.noscience.net/home/iforaker/svn/libtpt/trunk@3 ad02c61d-1c37-db11-a760-0003470a40ca

--- src/buffer.cxx
+++ src/buffer.cxx
@@ -35,14 +35,61 @@ private:
 struct Buffer::pimpl {
 	bool m_fileopen;
 	bool m_bufferalloc;
+	bool m_closefilewhendone;
+	
+	fstream* m_filein;
+
 	char* m_buffer;
 	int m_bufsize;
 	int m_bufindex;
-	fstream* m_filein;
-
+	
 	bool openfile(char* filename);
 	bool readfile();
+
+	pimpl() : m_fileopen(false), m_bufferalloc(false), m_closefilewhendone(false) {}
+	~pimpl();
+	
 }; // end struct Buffer::pimpl
+
+Buffer::Buffer()
+{
+	m_pimpl = new pimpl;
+
+}
+
+Buffer::Buffer(char* filename)
+{
+	m_pimpl = new pimpl;
+
+	m_pimpl->openfile(filename);
+}
+
+Buffer::Buffer(std::fstream* filein)
+{
+	m_pimpl = new pimpl;
+
+	m_pimpl->m_fileopen = true;
+	m_pimpl->m_filein = filein;
+}
+
+Buffer::~Buffer()
+{
+	delete m_pimpl;
+}
+
+Buffer::pimpl::~pimpl()
+{
+	if (m_closefilewhendone)
+	{
+		delete m_filein;
+	}
+}
+
+bool Buffer::pimpl::openfile(char* filename)
+{
+	m_filein = new fstream(filename);
+	return false;
+}
 
 
 } // end namespace TPTLib
